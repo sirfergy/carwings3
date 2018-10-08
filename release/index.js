b13d2065b251fc9e5b50890c110d7c3e80244c85
sirfergy <sean@thefergies.net>
2018-10-07 17:49:32 -0700

    move to lib instead of release and remove generated files

--- release/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const service_1 = require("./service");
-exports.Service = service_1.Service;
-//# sourceMappingURL=index.js.map
